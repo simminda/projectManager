3556717ec3b39026f0546dd9306d4a9f8c067cff
simphiwen <simtabz@gmail.com>
2021-08-10 17:17:26 +0200

    first version

---
+++ src/poised/Project.java
@@ -0,0 +1,55 @@
+package poised;
+
+public class Project {
+	// Attributes
+	Integer projectNumber;
+	String projectName;
+	String buildingType;
+	String projectAddress;
+	String erfNumber;
+	Double projectFee;
+	Double projectBalance;
+	String deadline;
+	Assignee architect;
+	Assignee contractor;
+	Assignee customer;
+	
+    // Methods
+    public Project(Integer projectNumber, String projectName, String buildingType, String projectAddress,
+    				String erfNumber, Double projectFee, Double projectBalance, String deadline) 
+    {
+    	this.projectNumber = projectNumber;
+    	this.projectName = projectName;
+    	this.buildingType = buildingType;
+    	this.projectAddress = projectAddress;
+    	this.erfNumber = erfNumber;
+    	this.projectFee = projectFee;
+    	this.projectBalance = projectBalance;
+    	this.deadline = deadline;
+    }
+    
+    public String toString() {
+		String output = "Project";
+		output += "\nProject Number\t: " + projectNumber;
+		output += "\nProject Name\t: " + projectName;
+		output += "\nBuilding Type\t: " + buildingType;
+		output += "\nProject Address\t: " + projectAddress;
+		output += "\nerf Number\t: " + erfNumber;
+		output += "\nProject Fee\t: " + projectFee;
+		output += "\nProject Balance\t: " + projectBalance;
+		output += "\nDeadline\t: " + deadline;
+			   
+		return output;
+	}
+    
+    // set the due date for project
+    public void setDeadline(String newDeadline) {
+    	deadline = newDeadline;
+    }
+    
+    // set the outstanding balance for project
+    public void setBalance(Double newBalance) {
+    	projectBalance = newBalance;
+    }
+
+}
